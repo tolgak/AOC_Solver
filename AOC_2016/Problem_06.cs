077f3aaa97c6786e78e505457c977c36e1ce7b0a
Tolga Kurkcuoglu <tolga.kurkcuoglu@gmail.com>
2021-12-04 18:51:28 +0300

    2016 day 6

--- AOC_2016/Problem_06.cs
+++ AOC_2016/Problem_06.cs
@@ -10,6 +10,7 @@ namespace AOC_2016
   public class Problem_06 : IProblem
   {
     private StringBuilder _logger = new();
+    private int[,] _freq = new int[8, 26];
 
     public int Day() => 6;
 
@@ -21,26 +22,46 @@ namespace AOC_2016
     public string Solve(int part, string input)
     {
       _logger.Clear();
+      var items = input.Split(Environment.NewLine);
+      for (int i = 0; i < items.Length; i++)
+      {
+        for (int j = 0; j < items[i].Length; j++)
+        {
+           int index = items[i].ElementAt(j) - 97;
+          _freq[j, index] += 1;
+        }
+      }
 
       if (part == 1)
-        return this.Solve_Part1(input);
+        return this.Solve_Part1();
 
-      return this.Solve_Part2(input);
+      return this.Solve_Part2();
     }
 
-    private string Solve_Part1(string input)
+    private string Solve_Part1(string input = "")
     {
+      var word = string.Empty;      
+      for (int j = 0; j < _freq.GetLength(0); j++)
+      {
+        var row = Matrix<int>.GetRow(_freq, j);
+        var value = Array.IndexOf(row, row.Max());
+        word += (char) (value + 97);
+      }
 
-
-
-      return "";
+      return word;
     }
 
-    private string Solve_Part2(string input)
+    private string Solve_Part2(string input = "")
     {
+      var word = string.Empty;
+      for (int j = 0; j < _freq.GetLength(0); j++)
+      {
+        var row = Matrix<int>.GetRow(_freq, j);
+        var value = Array.IndexOf(row, row.Min());
+        word += (char)(value + 97);
+      }
 
-
-      return "";
+      return word;
     }
 
 
